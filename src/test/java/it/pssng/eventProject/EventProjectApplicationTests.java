2d56926f9ea0bd00b2a80941ef81d8b9eacf0df5
Paolo Coletta <112562401+paolinucs@users.noreply.github.com>
2023-11-27 15:30:06 +0100

    first commit, empty project

---
+++ src/test/java/it/pssng/eventProject/EventProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.pssng.eventProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
