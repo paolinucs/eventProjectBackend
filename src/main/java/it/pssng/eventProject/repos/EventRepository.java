7041a820d75957e36fdba497bab178cfdcfe6e35
Paolo <public@pcdev.it>
2024-01-05 12:34:06 +0000

    bug fix

--- src/main/java/it/pssng/eventProject/repos/EventRepository.java
+++ src/main/java/it/pssng/eventProject/repos/EventRepository.java
@@ -7,8 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 
 import it.pssng.eventProject.model.Event;
 
-public interface EventRepository extends JpaRepository<Event,Long> {
+public interface EventRepository extends JpaRepository<Event, Long> {
 
-    @Query(value="SELECT * FROM Event WHERE Event.eventCategory = ?1")
-    public List<Event> findAllEventsByCategory(String category);
+    @Query(value = "SELECT e FROM Event e WHERE e.eventCategory = ?1")
+    List<Event> findAllEventsByCategory(String category);
 }
